77264706213ea020ac7f47fcef890e77ced866a8
Cassandra Bazille <bazillecassandra@gmail.com>
2025-02-17 20:17:11 +0100

    initial structure

---
+++ script.js
@@ -0,0 +1,11 @@
+//dons : Une variable mensuelle et un variable montant, mettre une valeur par défaut à 20 euros mensuel par ex //
+
+
+
+//photos (exo aspirateur): Quand je clique sur Oscar, l'image d'Oscar s'affiche en grand.4 parametres image nom de l'image, deuxieme texte, troisieme texte //
+
+
+//testez vos connaissances : utiliser inner html//
+
+
+//formulaire : Variable si on répond aux trois questions il affiche le score //
